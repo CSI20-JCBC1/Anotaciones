2cfecc5bd50a14dfd3969b0e0b0afc6aad8e035e
CSI20-JCBC1 <csi20-jbadrbo@colegioaltair.net>
2022-11-10 12:55:59 +0100

    Proyecto clase

---
+++ Anotaciones/src/main/java/com/app/anota/ServicioA.java
@@ -0,0 +1,10 @@
+package com.app.anota;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioA {
+
+	public String getMensaje() {
+		return("Enviando mensaje de clase Servicio A");
+	}
+}
